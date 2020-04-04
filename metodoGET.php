ae24808f253100ef318dbd0bda2b468d564cf645
Juan <juan.gallegomesa@gmail.com>
2020-04-04 10:13:50 -0500

    ejemplo post y get

---
+++ metodoGET.php
@@ -0,0 +1,12 @@
+<?php
+
+print_r($_GET);
+
+$nombre = $_GET['nombre'];
+echo ("<br> Su nombre es: ".$nombre);
+
+
+
+
+
+?>
---
+++ metodoPOST.php
@@ -0,0 +1,15 @@
+<?php
+
+print_r($_POST);
+
+$nombre = $_POST['nombre2'];
+$edad = $_POST['edad2'];
+echo ("<br> Su nombre es: ".$nombre);
+echo ("<br> Su edad es: ".$edad);
+
+
+
+
+
+
+?>
